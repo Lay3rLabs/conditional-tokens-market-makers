96834bec7d6b6ef33fa3e2679070c351131fbf65
Noah Saso <noahsaso@gmail.com>
2025-01-29 18:57:25 +0100

    continued porting

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -2,15 +2,11 @@
 pragma solidity ^0.8.22;
 
 contract Migrations {
-    address public owner;
+    address public owner = msg.sender;
     uint public lastCompletedMigration;
 
     modifier restricted() {
         if (msg.sender == owner) _;
-    }
-
-    constructor() public {
-        owner = msg.sender;
     }
 
     function setCompleted(uint completed) public restricted {
--- contracts/Whitelist.sol
+++ contracts/Whitelist.sol
@@ -1,28 +1,29 @@
 // SPDX-License-Identifier: LGPL-3.0
 pragma solidity ^0.8.22;
 
-import 'openzeppelin-solidity/contracts/ownership/Ownable.sol'; 
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
 /// @title Whitelist for adding/removing users from a whitelist
 /// @author Anton Shtylman - @InfiniteStyles
 contract Whitelist is Ownable {
+    event UsersAddedToWhitelist(address[] users);
+    event UsersRemovedFromWhitelist(address[] users);
 
-  event UsersAddedToWhitelist(address[] users);
-  event UsersRemovedFromWhitelist(address[] users);
+    mapping(address => bool) public isWhitelisted;
 
-  mapping(address => bool) public isWhitelisted;
+    constructor(address initialOwner) Ownable(initialOwner) {}
 
-  function addToWhitelist(address[] calldata users) onlyOwner external {
-    for (uint i = 0; i < users.length; i++) {
-      isWhitelisted[users[i]] = true;
+    function addToWhitelist(address[] calldata users) external onlyOwner {
+        for (uint i = 0; i < users.length; i++) {
+            isWhitelisted[users[i]] = true;
+        }
+        emit UsersAddedToWhitelist(users);
     }
-    emit UsersAddedToWhitelist(users);
-  }
 
-  function removeFromWhitelist(address[] calldata users) onlyOwner external {
-    for (uint i = 0; i < users.length; i++) {
-      isWhitelisted[users[i]] = false;
+    function removeFromWhitelist(address[] calldata users) external onlyOwner {
+        for (uint i = 0; i < users.length; i++) {
+            isWhitelisted[users[i]] = false;
+        }
+        emit UsersRemovedFromWhitelist(users);
     }
-    emit UsersRemovedFromWhitelist(users);
-  }
 }
